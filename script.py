324f0446031eb65ee8456db0fd0ef8a300eb18e2
afa87192 <86274180+afa87192@users.noreply.github.com>
2021-07-10 07:55:02 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ if y =="Русский"or "русский" or"руский"  or 'Russian ':
                if what ==  "+" :
                     a  =  float ( input ( "Первое слогаемое " ))
                     b  =  float ( input ( "Второе слогаемое " )) 
-                    print ( "Результат :"+  str (  a  +  b ))
+                    print ( "Результат :", a  +  b (sep = ''))
                elif what ==  "-" :
                     a  =  float ( input ( "Уменьшаймое" ))
                     b  =  float ( input ( "Вычитаймое" ))  
@@ -39,27 +39,27 @@ if y =="Русский"or "русский" or"руский"  or 'Russian ':
                if what ==  "прямоугольник" :
                     a  =  float ( input ( "Первая сторона" ))
                     b  =  float ( input ( "Вторая сторона" )) 
-                    print ( "Результат периметр  :", a  +  b,'см плошадь:',a*b," см2")
+                    print ( "Результат периметр  :", a  +  b,'см плошадь:',a*b," см2"(sep = ''))
                elif what ==  "квадрад" :
                     a  =  float ( input ( " сторона" ))
-                    print (  "Результат периметр  :", a  +  a,'см плошадь:',a*a," см2")
+                    print (  "Результат периметр  :", a  +  a,'см плошадь:',a*a," см2"(sep = ''))
                     
                elif what  ==  "окружность" :
                     a  =  float ( input ( "радиус" ))  
-                    print ( "Результат периметр  :",  a*2*3.14,'см плошадь:',a**2*3.14," см2")
+                    print ( "Результат периметр  :",  a*2*3.14,'см плошадь:',a**2*3.14," см2"(sep = ''))
           if what == '3д':
                what  =  input ( 'какая фигура (куб, шар,цилиндр, параллелепипед)')
                if what ==  "шар" :
                     r=int(input ('радиус'))
-                    print ( "Результат плошадь:'", 4* 3.14*r**2," см2 объём ",3/4*3.14*r**3 )  
+                    print ( "Результат плошадь:'", 4* 3.14*r**2," см2 объём ",3/4*3.14*r**3 (sep = ''))  
                if what ==  " куб" :
                     a  =  float ( input ( "Сторона" ))
-                    print ( "Результат периметр  :",  4*(a + a + a) ,'см плошадь:', 2*( a* a +   a*a +  a*a)," см2 объём ",a **3 )
+                    print ( "Результат периметр  :",  4*(a + a + a) ,'см плошадь:', 2*( a* a +   a*a +  a*a)," см2 объём ",a **3(sep = '') )
                if what ==  " параллелепипед" :
                     a  =  float ( input ( "Первая сторона" ))
                     b  =  float ( input ( "Вторая сторона" ))
                     с = float ( input ( "Третья сторона" ))
-                    print ( "Результат периметр  :",  4*(a + b + с) ,'см плошадь:', 2*( a* b +   b*с +  a*с)," см2 объём ",a * b * с )
+                    print ( "Результат периметр  :",  4*(a + b + с) ,'см плошадь:', 2*( a* b +   b*с +  a*с)," см2 объём ",a * b * с (sep = ''))
           else:
                print ( "Неверная операция !! " )     
      if what == 'скорость':
@@ -76,3 +76,81 @@ if y =="Русский"or "русский" or"руский"  or 'Russian ':
                s=int(input( 'растояние?'))
                t=int(input( 'время?'))
                print(s/t)  
+if y =="English ":
+     print ('''
+                     ______________________
+                    |                      |
+                    | Calculator v1.03     |
+                    | ____________________ |
+           support for signs (*, +, -, **) and calculation of area, perimeter ''')
+     what = input ('what do you need geometry, arithmetic, speed?')
+     if what == "arithmetic":
+          what = input ("Action sign (*,:, +, -, **)")
+          if what == '*' or ':' or '+' or '-' or '**':
+               if what == "+":
+                    a = float (input ("First syllable"))
+                    b = float (input ("Second syllable"))
+                    print ("Result:" + str (a + b))
+               elif what == "-":
+                    a = float (input ("Reduced"))
+                    b = float (input ("Subtracted"))
+                    print ("Result:" + str (a - b))
+               elif what == "*":
+                    a = float (input ("First factor"))
+                    b = float (input ("Second factor"))
+                    print ("Result:" + str (a * b))
+               elif what == ":":
+                    a = float (input ("dividend"))
+                    b = float (input ("divisor"))
+                    print ("Result:" + str (a / b))
+               elif what == "**":
+                    a = float (input ("First number"))
+                    b = float (input ("Second number"))
+                    print ("Result:" + str (a ** b))
+               else:
+                    print ("Invalid operation !!")
+     if what == 'geometry':
+          what = input ('3d or 2d shapes?')
+          if what == '2d':
+               what = input ('what shape (rectangle, square, circle')
+               if what == "rectangle":
+                    a = float (input ("First side"))
+                    b = float (input ("Second side"))
+                    print ("Result perimeter:", a + b, 'cm area:', a * b, "cm2"(sep = ''))
+               elif what == "quadrad":
+                    a = float (input ("side"))
+                    print ("Result perimeter:", a + a, 'cm area:', a * a, "cm2"(sep = ''))
+                    
+               elif what == "circle":
+                    a = float (input ("radius"))
+                    print ("Result perimeter:", a * 2 * 3.14, 'cm area:', a ** 2 * 3.14, "cm2"(sep = ''))
+          if what == '3d':
+               what = input ('what shape (cube, ball, cylinder, parallelepiped)')
+               if what == "ball":
+                    r = int (input ('radius'))
+                    print ("Result area: '", 4 * 3.14 * r ** 2, "cm2 volume", 3/4 * 3.14 * r ** 3(sep = ''))
+               if what == "cube":
+                    a = float (input ("Side"))
+                    print ("Result perimeter:", 4 * (a + a + a), 'cm area:', 2 * (a * a + a * a + a * a), "cm2 volume", a ** 3(sep = ''))
+               if what == "box":
+                    a = float (input ("First side"))
+                    b = float (input ("Second side"))
+                    c = float (input ("third party"))
+                    print ("Result perimeter:", 4 * (a + b + c), 'cm area:', 2 * (a * b + b * c + a * c), "cm2 volume", a * b * c (sep = ''))
+          else:
+                    print ("Invalid operation !!")
+     if what == 'speed':
+          x = input ('what are we looking for? time? speed? distance?')
+          if x == 'time':
+               s = int (input ('distance?'))
+               v = int (input ('speed?'))
+               print (s / v)
+          if x == 'distance':
+               v = int (input ('speed?'))
+               t = int (input ('time?'))
+               print (v * t)
+          if x == 'speed':
+               s = int (input ('distance?'))
+               t = int (input ('time?'))
+               print (s / t)     
+     
